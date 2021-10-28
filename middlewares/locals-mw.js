80f4aef9e84b40bef313dc54c485dc166d928fd6
1binibin <namwonbin@gmail.com>
2021-10-28 15:08:28 +0900

    수정

--- middlewares/locals-mw.js
+++ middlewares/locals-mw.js
@@ -1,23 +1,25 @@
+const path = require('path');
 const linkInit = require('../modules/link-init');
+
 module.exports = (req, res, next) => {
   res.locals.user = req.user || null;
   res.locals.path = {};
-  res.locals.init = linkInit.admin;
-  res.locals.current = req.path;
-  res.locals.currents = req.path.split('/');
-  res.locals.currents.shift();
-  const { currents } = res.locals.path;
+  res.locals.path.init = linkInit.admin;
+  res.locals.path.current = req.path;
+  res.locals.path.currents = req.path.split('/');
+  res.locals.path.currents.shift();
   res.locals.path.second = '/' + res.locals.path.currents[0];
   res.locals.path.second += res.locals.path.currents[1]
     ? '/' + res.locals.path.currents[1]
     : '';
+  console.log(res.locals.path.current, res.locals.path.second);
   next();
 };
 
 /* 
-path: {
+path : {
   current: 현재경로
-  currents: 경로 배열 
-  second: 2Depth 경로
+  currents: 경로 배열
+  second: 2depth 경로
 }
 */
