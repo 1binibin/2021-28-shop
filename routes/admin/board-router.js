082abd8c1bd15fe2580f3543c20c22941e4eba8e
1binibin <namwonbin@gmail.com>
2021-10-28 13:25:35 +0900

    board design 중

--- routes/admin/board-router.js
+++ routes/admin/board-router.js
@@ -3,8 +3,18 @@ const express = require('express');
 const router = express.Router();
 const { error } = require('../../modules/util');
 
+router.get('/init', (req, res, next) => {
+  res.render('admin/board/board-init', { css: 'admin-board' });
+});
+
 router.get('/', (req, res, next) => {
-  res.render('admin/board/board-list', { css: 'admin-board' });
+  const boardType = req.query.type || 'default';
+  res.render('admin/board/board-list', { css: 'admin-board', boardType });
+});
+
+router.get('/:id', (req, res, next) => {
+  const boardType = req.query.type || 'default';
+  res.render('admin/board/board-view', { css: 'admin-board', boardType });
 });
 
 router.post('/', (req, res, next) => {
@@ -17,10 +27,6 @@ router.put('/', (req, res, next) => {
 
 router.delete('/', (req, res, next) => {
   res.send('/admin/board:DELETE');
-});
-
-router.get('/init', (req, res, next) => {
-  res.render('admin/board/board-init', { css: 'admin-board' });
 });
 
 router.post('/init', (req, res, next) => {
