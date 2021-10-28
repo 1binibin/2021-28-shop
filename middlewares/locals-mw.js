62e135da6be3b40a51e948358e1d51b162059f3d
1binibin <namwonbin@gmail.com>
2021-10-28 15:02:31 +0900

    에러 처리전

--- middlewares/locals-mw.js
+++ middlewares/locals-mw.js
@@ -1,18 +1,23 @@
 const linkInit = require('../modules/link-init');
 module.exports = (req, res, next) => {
   res.locals.user = req.user || null;
-  res.locals.links = linkInit.admin;
-  res.locals.currentPath = req.originalUrl;
-  const currentPaths = req.originalUrl.split('/');
-  currentPaths.shift();
-  res.locals.currentPaths = currentPaths;
-  res.locals.secondPath =
-    '/' + currentPaths[0] + (currentPaths[1] ? '/' + currentPaths[1] : '');
+  res.locals.path = {};
+  res.locals.init = linkInit.admin;
+  res.locals.current = req.path;
+  res.locals.currents = req.path.split('/');
+  res.locals.currents.shift();
+  const { currents } = res.locals.path;
+  res.locals.path.second = '/' + res.locals.path.currents[0];
+  res.locals.path.second += res.locals.path.currents[1]
+    ? '/' + res.locals.path.currents[1]
+    : '';
   next();
 };
 
 /* 
-currentPath = 현재경로 originalUrl
-currentPaths = 현재경로 배열
-secondPath = /admin/board
+path: {
+  current: 현재경로
+  currents: 경로 배열 
+  second: 2Depth 경로
+}
 */
