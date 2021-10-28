93d8adcefa1aa7f98b8bc452d314d04af546e3a7
1binibin <namwonbin@gmail.com>
2021-10-28 16:10:50 +0900

    router 재설계

--- routes/admin/binit-router.js
+++ routes/admin/binit-router.js
@@ -4,19 +4,19 @@ const router = express.Router();
 const { error } = require('../../modules/util');
 
 router.get('/init', (req, res, next) => {
-  res.render('admin/cate/cate', { css: 'admin-cate' });
+  res.render('admin/board/board-init', { css: 'admin-board' });
 });
 
 router.put('/', (req, res, next) => {
-  res.send('/admin/cate:PUT');
+  res.send('/admin/binit:PUT');
 });
 
 router.delete('/', (req, res, next) => {
-  res.send('/admin/cate:DELETE');
+  res.send('/admin/binit:DELETE');
 });
 
 router.post('/init', (req, res, next) => {
-  res.send('/admin/cate/init');
+  res.send('/admin/binit/init');
 });
 
-module.exports = { name: '/cate', router };
+module.exports = { name: '/binit', router };
---
+++ routes/admin/logout-router.js
@@ -0,0 +1,10 @@
+const path = require('path');
+const express = require('express');
+const router = express.Router();
+const { error } = require('../../modules/util');
+
+router.get('/', (req, res, next) => {
+  res.render('logout');
+});
+
+module.exports = { name: '/logout', router };
