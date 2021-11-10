653ca1226d73e72913188d443350789e2e81f997
1binibin <namwonbin@gmail.com>
2021-11-10 12:52:23 +0900

    에러

--- public/js/cate/cate.js
+++ public/js/cate/cate.js
@@ -1,10 +1,12 @@
-/* var core = {};
-var plugins = ['contextmenu', 'dnd', 'state', 'wholerow', 'changed'];
+var core = {};
+var plugins = ['contextmenu', 'dnd', 'search', 'state', 'wholerow', 'changed'];
 
 core.themes = {
-  variant: true,
+  variant: 'large',
   striped: true,
 };
+
+core.check_callback = true;
 
 core.data = {
   url: function (node) {
@@ -16,43 +18,15 @@ core.data = {
 };
 
 function onChangedTree(e, data) {
-  console.log(data.changed.selected); // newly selected
-  console.log(data.changed.deselected); // newly deselected
+  var json = $('#jstreeWrap').jstree(true).get_json('#');
+  axios
+    .post('/api/tree', { json })
+    .then(function (r) {
+      console.log(r);
+    })
+    .catch(function (err) {
+      console.log(err);
+    });
 }
 
-$('#jstreeWrap').on('changed.jstree', onChangedTree).jstree({ core: core, plugins: plugins });
- */
-
-$('#jstreeWrap').jstree({
-  core: {
-    animation: 0,
-    check_callback: true,
-    themes: { stripes: true },
-    data: {
-      url: function (node) {
-        return node.id === '#' ? '/ajax_demo_roots.json' : '/ajax_demo_children.json';
-      },
-      data: function (node) {
-        return { id: node.id };
-      },
-    },
-  },
-  types: {
-    '#': {
-      max_children: 1,
-      max_depth: 4,
-      valid_children: ['root'],
-    },
-    root: {
-      valid_children: ['default'],
-    },
-    default: {
-      valid_children: ['default', 'file'],
-    },
-    file: {
-      icon: 'glyphicon glyphicon-file',
-      valid_children: [],
-    },
-  },
-  plugins: ['contextmenu', 'dnd', 'search', 'state', 'types', 'wholerow'],
-});
+$('#jstreeWrap').jstree({ core: core, plugins: plugins }).on('changed.jstree', onChangedTree);
--- routes/api/tree-router.js
+++ routes/api/tree-router.js
@@ -3,10 +3,10 @@ const fs = require('fs-extra');
 const express = require('express');
 const router = express.Router();
 
-router.get('/', async (req, res, next) => {
+router.post('/', async (req, res, next) => {
   try {
-    const tree = await fs.readJson(path.join(__dirname, '../../json/tree.json'));
-    res.status(200).json(tree);
+    const tree = await fs.writeJSON(path.join(__dirname, '../../json/tree.json'), req.body.json);
+    res.status(200).json({ success: true });
   } catch (err) {
     res.status(500).json(err);
   }
