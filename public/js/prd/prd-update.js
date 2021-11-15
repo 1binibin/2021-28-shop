1ed7c46a633fb04fb6311be3b3b456bbd55bb54b
1binibin <namwonbin@gmail.com>
2021-11-15 16:04:36 +0900

    Product update 작업중

--- public/js/prd/prd-update.js
+++ public/js/prd/prd-update.js
@@ -93,6 +93,7 @@ var quill = new Quill('#editor', {
   theme: 'snow',
 });
 
+// 파일 삭제
 $('form[name="prdCreateForm"]').submit(onSubmitPrdCreateForm);
 function onSubmitPrdCreateForm(e) {
   e.preventDefault();
@@ -103,4 +104,24 @@ function onSubmitPrdCreateForm(e) {
   }
   this.content.value = quill.root.innerHTML;
   this.submit();
+}
+
+function onDeleteFile(id, el) {
+  if (confirm('파일을 삭제하시겠습니까?\n삭제하신 파일은 되돌릴 수 없습니다.')) {
+    axios
+      .delete('/admin/api/file/' + id + '?modelName=ProductFile')
+      .then(onSuccess)
+      .catch(onError);
+  }
+  function onSuccess(r) {
+    if (r.data.code == 200) {
+      var html = '<input type="file" name="' + $(el).data('name') + '" class="form-control-file mb-2" />';
+      $(el).parent().after(html); // $(el).parent().parent().append(html);
+      $(el).parent().remove();
+    }
+  }
+  function onError(err) {
+    console.log(err);
+    console.log(err.response);
+  }
 }
--- routes/admin/api-router.js
+++ routes/admin/api-router.js
@@ -2,13 +2,14 @@ const express = require('express');
 const router = express.Router();
 const createError = require('http-errors');
 const { moveFile, alert } = require('../../modules/util');
-const { BoardFile } = require('../../models');
+const db = require('../../models');
 
 router.delete('/file/:id', async (req, res, next) => {
   try {
+    const modelName = req.query.modelName || 'BoardFile';
     const { id } = req.params;
-    const { saveName } = await BoardFile.findOne({ where: { id }, attributes: ['saveName'] });
-    await BoardFile.destroy({ where: { id } });
+    const { saveName } = await db[modelName].findOne({ where: { id }, attributes: ['saveName'] });
+    await db[modelName].destroy({ where: { id } });
     await moveFile(saveName);
     res.status(200).json({ code: 200, result: 'success' });
   } catch (err) {
